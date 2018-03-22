dcf46286f693d86f2b8586531dc99c8a557604fe
Shafique Arnab <shafique@buncee.com>
2018-03-21 20:23:29 -0400

    Better variable naming

--- max_sum_nonadjacent.py
+++ max_sum_nonadjacent.py
@@ -11,22 +11,22 @@ def max_sum_nonadjacent(values):
 	Returns:
 		The maximum sum of two non-adjacent numbers in the sequence.
 	"""
-	running_max = 0
-	prev_max = 0
+	prev_max_incl = 0
+	prev_max_excl = 0
     
 	for i in values:
 		# Between the last two maximums, what is the larger one?
-		next_max = max(prev_max, running_max)
+		next_max = max(prev_max_incl, prev_max_excl)
         
-        	# Include the current value in the sum for comparison on the
-        	# next iteration
-		prev_max = running_max + i
+		# Include the current value in the sum for comparison on the
+		# next iteration
+		prev_max_incl = prev_max_excl + i
 
 		# Save the running maximum excluding the current value
 		# prevent adjacent sums
-		running_max = next_max
+		prev_max_excl = next_max
 
-	return max(prev_max, running_max)
+	return max(prev_max_incl, prev_max_excl)
 
 assert max_sum_nonadjacent([2, 4, 6, 8]) == 12
 assert max_sum_nonadjacent([5, 1, 1, 5]) == 10
